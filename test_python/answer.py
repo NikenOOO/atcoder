25acaf36b6e84674565b493cc5e851c666f684cf
NikenOOO <kouhei23.0927@gmail.com>
2021-05-16 21:13:35 +0900

    Atcoder 201

--- test_python/answer.py
+++ test_python/answer.py
@@ -1,52 +1,27 @@
-N = int(input())
-C = list(map(int, input().split()))
-Q = int(input())
+S = input()
+ok = []
+hatena = []
+
+for i in range(0, 10):
+  if S[i] == "o":
+    ok.append(i)
+  elif S[i] == "?":
+    hatena.append(i)
 
 ans = 0
 
-min_odd = 10000000000
-min_all = 10000000000
+if len(ok) >= 5:
+  ans = 0
+elif len(ok) == 4:
+  ans = 24
+elif len(ok) == 3:
+  ans = 36 + 24*len(hatena)
+elif len(ok) == 2:
+  ans = 14 + 24 * len(hatena) + 12* len(hatena)**2
+elif len(ok) == 1:
+  ans = 1 + 4 * len(hatena)**3 + 4 * len(hatena) + 6 * len(hatena)**2
 
-for i in range(0, N):
-  if i % 2 == 0:
-    min_odd = min(min_odd, C[i])
-  else:
-    min_all = min(min_all, C[i])
-
-dec_o = 0
-dec_a = 0
-
-for _ in range(0, Q):
-  S = list(map(int, input().split()))
-  # 単品販売
-  if S[0] == 1:
-    x = S[1]
-    a = S[2]
-    if x % 2 != 0:
-      if C[x-1] - dec_a - dec_o >= a:
-        C[x-1] = C[x-1] - a
-        min_odd = min(min_odd, C[x-1])
-        ans += a
-    else: 
-      if C[x-1] - dec_a >= a:
-        C[x-1] = C[x-1] - a
-        min_all = min(min_all, C[x-1])
-        ans += a
-  # セット販売
-  elif S[0] == 2:
-    a = S[1]
-    if min_odd - dec_a - dec_o >= a:
-      dec_o += a
-  # 全種類販売
-  elif S[0] == 3:
-    a = S[1]
-    if min(min_all - dec_a, min_odd - dec_a -dec_o) >= a:
-      dec_a += a
-
-for i in range(0, N):
-  if i % 2 == 0:
-    ans += dec_o
-
-ans += N*dec_a
+if ans == 0 and len(ok) == 0:
+  ans = len(hatena)**4
 
 print(ans)
