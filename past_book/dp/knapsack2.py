aed43854bc60635872abdf6fa0239d73ac4d3a98
NikenOOO <kouhei23.0927@gmail.com>
2021-06-19 00:21:10 +0900

    knapsack

--- past_book/dp/knapsack2.py
+++ past_book/dp/knapsack2.py
@@ -1,3 +1,4 @@
+# 問題：https://atcoder.jp/contests/dp/tasks/dp_e
 
 N, W = list(map(int, input().split()))
 
@@ -9,7 +10,7 @@ for i in range(N):
   ws.append(w)
   vs.append(v)
 
-v_range = N*5
+v_range = 100000
 weight = []
 for i in range(N+1):
   weight.append([10**9+1]*(v_range+1))
@@ -22,7 +23,6 @@ for i in range(1, N+1):
     weight[i][v] = min(weight[i][v], weight[i-1][v])
     if v - vs[i] >= 0:
       weight[i][v] = min(weight[i][v], weight[i-1][v-vs[i]]+ws[i])
-print(weight)
 
 ans = 0
 for i in range(N+1):
