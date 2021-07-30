da6867b63c20c97b53f8f786fcdc92ab21855f10
NikenOOO <kouhei23.0927@gmail.com>
2021-07-30 14:29:46 +0900

    refresh

--- test_python/answer.py
+++ test_python/answer.py
@@ -1,33 +1,11 @@
+H, Q = map(int, input().split())
 
-N, W = list(map(int, input().split()))
+ans = []
 
-ws = [0]
-vs = [0]
+for _ in range(H):
+  C = input()
+  ans.append(C)
+  ans.append(C)
 
-for i in range(N):
-  w, v = list(map(int, input().split()))
-  ws.append(w)
-  vs.append(v)
-
-v_range = N*5
-weight = []
-for i in range(N+1):
-  weight.append([10**9+1]*(v_range+1))
-
-
-weight[0][0] = 0
-
-for i in range(1, N+1):
-  for v in range(v_range+1):
-    weight[i][v] = min(weight[i][v], weight[i-1][v])
-    if v - vs[i] >= 0:
-      weight[i][v] = min(weight[i][v], weight[i-1][v-vs[i]]+ws[i])
-print(weight)
-
-ans = 0
-for i in range(N+1):
-  for j in range(v_range+1):
-    if weight[i][j] <= W and j >= ans:
-      ans = j
-
-print(ans)
+for i in range(len(ans)):
+  print(ans[i])
